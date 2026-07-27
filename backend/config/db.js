9e8cb3b843504e8b9b8d72f960b190efad7278eb
Sisouko <ahmedabdrebbi48@gmail.com>
2026-07-27 18:17:10 +0100

    initialisons la connexion à PostgreSQL via Sequelize.

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+const { sequelize } = require('sequelize')
+require('dotenv').config()
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging : false,
+})
+
+module.exports = sequelize
